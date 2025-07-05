6c2cb8622a497025cafdf99d04d9161c5633b714
shubhrakantighosh1 <shubhrakantighosh@1996.com>
2025-07-06 01:53:41 +0530

    add json tag

--- internal/model/auth_token.go
+++ internal/model/auth_token.go
@@ -6,13 +6,13 @@ import (
 )
 
 type AuthToken struct {
-	ID               uint64
-	UserID           uint64
-	AccessToken      string
-	RefreshToken     string
-	AccessExpiresAt  time.Time
-	RefreshExpiresAt time.Time
-	CreatedAt        time.Time
-	UpdatedAt        time.Time
-	DeletedAt        gorm.DeletedAt
+	ID               uint64         `json:"id"`
+	UserID           uint64         `json:"user_id"`
+	AccessToken      string         `json:"access_token"`
+	RefreshToken     string         `json:"refresh_token"`
+	AccessExpiresAt  time.Time      `json:"access_expires_at"`
+	RefreshExpiresAt time.Time      `json:"refresh_expires_at"`
+	CreatedAt        time.Time      `json:"created_at"`
+	UpdatedAt        time.Time      `json:"updated_at"`
+	DeletedAt        gorm.DeletedAt `json:"deleted_at"`
 }
